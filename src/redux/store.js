3bda8f317f344d19af0e0f8075399b6ac3841637
Michael Javier Mota <37409145+michaeljav@users.noreply.github.com>
2022-04-20 22:51:00 -0400

    Finished and cleaned code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,15 +1,12 @@
-// import {createStore} from 'redux';
-// import {createStore} from "redux";
-// import { createStore } from 'redux'
-// import { createStore } from Redux;
+
 const  {createStore} = Redux;
 
 import reducer from './reducer.js';
 
-// const store = Redux.createStore(reducer); 
+
 const store = createStore(reducer); 
-// const store = {}//createStore(reducer);
+
 
 export default store;
-// export default {};
+
 
